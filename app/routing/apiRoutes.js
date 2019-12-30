bf417f7f541e9253206d5c4c896e81b8375d97b0
Tulus Tiranda <tulustiranda@gmail.com>
2019-12-30 00:34:11 -0800

    final code

---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,46 @@
+var path = require("path");
+
+var friends = require("../data/friends");
+
+module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res){
+        res.json(friends);
+    });
+
+    app.post("/api/friends", function(req, res){
+
+        var user = req.body;
+
+        var response = user.scores;
+
+        var nameMatch = '';
+        var imageMatch = '';
+        var totalDiff = 500;
+
+     for( var i = 0; i < friends.length; i++){
+         var diff = 0;
+
+        for (var x = 0; x < response.length; x++){
+            diff += Math.abs(friends[i].scores[x] - response[x]);
+            console.log("Diff" + diff)
+        }
+
+        if (diff < totalDiff) {
+            totalDiff = diff;
+            console.log("Total Diff: " + totalDiff)
+            nameMatch = friends[i].name;
+            console.log("Name: " + nameMatch)
+            imageMatch = friends[i].photo;
+            console.log("Image: " + imageMatch)
+
+        }
+     }
+
+     friends.push(user);
+
+     res.json({status: "OK", nameMatch: nameMatch, imageMatch: imageMatch});
+     console.log("nameMatch: " + nameMatch)
+     console.log("imageMatch: " + imageMatch)
+    })
+}
---
+++ server.js
@@ -0,0 +1,18 @@
+  
+var express = require("express");
+var apiRoutes = require("./app/routing/apiRoutes");
+var htmlRoutes = require("./app/routing/htmlRoutes");
+var app = express();
+
+var PORT = process.env.PORT || 3000;
+
+app.use(express.static("public"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+apiRoutes(app);
+htmlRoutes(app);
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+});
